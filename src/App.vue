e80c9d38102eb15ac1a8e0c42faad6e73ce8d421
Alberto Gualis <agualis@gmail.com>
2017-05-11 10:30:32 +0200

    Sync router and Vuex

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,18 @@
 
 <script>
 import Vue from 'vue';
+import { sync } from 'vuex-router-sync';
 import { SweetModal } from 'sweet-modal-vue';
 import { store } from './vuex/store';
+import router from './router';
 import Spinner from './components/Spinner';
 import ErrorModal from './components/ErrorModal';
 
 Vue.component('spinner', Spinner);
 Vue.component('error-modal', ErrorModal);
 Vue.component('sweet-modal', SweetModal);
+
+sync(store, router);
 
 export default {
   name: 'app',
